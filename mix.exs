f69d5a149a35910f0554ff19534a272be31df6d5
Davide Bettio <davide.bettio@ispirata.com>
2020-03-26 11:12:17 +0100

    Prepare publish to hex.pm
    Add package() to mix.exs and ex_doc dependency.

--- mix.exs
+++ mix.exs
@@ -8,6 +8,7 @@ defmodule XMLStreamWriter.MixProject do
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      package: package(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         coveralls: :test,
@@ -20,13 +21,25 @@ defmodule XMLStreamWriter.MixProject do
 
   def application do
     [
-      extra_applications: [:logger]
     ]
   end
 
   defp deps do
     [
-      {:excoveralls, "~> 0.11", only: :test}
+      {:excoveralls, "~> 0.11", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+    ]
+  end
+
+  defp package do
+    [
+      description: "XML stream writer library",
+      maintainers: ["Davide Bettio"],
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => "https://github.com/ispirata/xml_stream_writer",
+        "Documentation" => "http://hexdocs.pm/xml_stream_writer"
+      }
     ]
   end
 end
